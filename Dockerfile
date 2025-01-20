d8c04590c68bd9c81ed47f1e1c3287146e4645a2
mathislindner <mathis.lindner@gmail.com>
2025-01-20 18:24:51 +0100

    added Dockerfile and updated logging and elp function

---
+++ Dockerfile
@@ -0,0 +1,20 @@
+# Use an official Python runtime as a parent image
+FROM python:3.11-slim
+
+# Install ffmpeg
+RUN apt-get update && apt-get install -y ffmpeg && rm -rf /var/lib/apt/lists/*
+
+# Set the working directory in the container
+WORKDIR /app
+
+# Copy the current directory contents into the container at /app
+COPY . /app
+
+# Install any needed packages specified in requirements.txt
+RUN pip install --no-cache-dir -r requirements.txt
+
+# Copy the .env file into the container
+COPY .env .env
+
+# Run app.py when the container launches
+CMD ["python", "main.py"]
--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-#from dotenv import load_dotenv
+from dotenv import load_dotenv
 import os
 import logging
 import yt_dlp
@@ -10,22 +10,16 @@ from telegram.ext import Updater, CommandHandler, MessageHandler, filters, Callb
 # https://gitlab.com/Athamaxy/telegram-bot-tutorial/-/blob/main/TutorialBot.py
 logger = logging.getLogger(__name__)
 
-# Pre-assign button text
-SELECT_GROUP = "Select a group"
-BACK_BUTTON = "Back"
-TUTORIAL_BUTTON = "Tutorial"
 
 def download_music(file_link: str, folder: str) -> str:
     """
     This function downloads the music from the link and saves it in the folder
     """
-
     ydl_opts = {
         'format': 'bestaudio/best',
         'outtmpl': os.path.join(folder, '%(title)s.%(ext)s'),
         'postprocessors': [{
             'key': 'FFmpegExtractAudio',
-            'ffmpeg_location': r'C:\Users\mathi\Documents\GH\telegram-bot-media-downloader\ffmpeg',
             'preferredcodec': 'mp3',
             'preferredquality': '192',
         }],
@@ -42,11 +36,10 @@ def process_link(update: Update, context: CallbackContext) -> None:
     This function would be added to the dispatcher as a handler for messages coming from the Bot API
     """
     # Print to console
-    print(f'{update.message.from_user.first_name} wrote {update.message.text}')
+    logger.info(f"Received a message from {update.message.chat_id}")
 
     file_info = {
         "file_link" : None,
-        "file_name" : None,
         "file_website" : None,
     }
     if not update.message.text:
@@ -56,25 +49,18 @@ def process_link(update: Update, context: CallbackContext) -> None:
     #verify if the message is a link
     if update.message.entities[0].type == "url":
         file_info["file_link"] = update.message.text
-        context.bot.send_message(
-            update.message.chat_id,
-            text="Successfully received the link! Processing...",
-            # To preserve the markdown, we attach entities (bold, italic...)
-            entities=update.message.entities
-            )
+        send_message_to_user(update, context, "Successfully received the link! Processing...")
         # cehck which website the link is from (youtube or soundcloud)
         if "youtube" in file_info["file_link"]:
             file_info["file_website"] = "youtube"
         elif "soundcloud" in file_info["file_link"]:
             file_info["file_website"] = "soundcloud"
         else:
-            update.message.reply_text(
-                "Sorry, I can only process links from YouTube and SoundCloud. Please try again.",
-                reply_markup=ForceReply(selective=True)
-            )
+            send_message_to_user(update, context, "Sorry, I can only download from Youtube and Soundcloud. Please send a valid link.")
             return
-        download_music(file_info["file_link"], "downloads")
-        
+        video_title = download_music(file_info["file_link"], f"downloads/{file_info['file_website']}/")
+        logger.info(f"Downloaded {video_title}")
+        send_message_to_user(update, context, f"Successfully downloaded {video_title}.")
         
     else:
         update.message.reply_text(
@@ -83,13 +69,21 @@ def process_link(update: Update, context: CallbackContext) -> None:
         )
         return
     
+def send_message_to_user(update: Update, context: CallbackContext, message: str) -> None:
+    """
+    This function sends a message to the user
+    """
+    update.message.reply_text(
+        message,
+        reply_markup=ForceReply(selective=True)
+    )
 
 def help(update: Update, context: CallbackContext) -> None:
     """
-    This function is called when the user sends the /start command
+    This function is called when the user sends the /help command
     """
     update.message.reply_text(
-        "Hello! I am a bot that can help you with your group management. Please select a group to start.",
+        "Hello! I am a bot that can help you with downloading music from Youtube and Soundcloud. Please send me a link to the music you want to download.",
         reply_markup=ForceReply(selective=True)
     )
 
@@ -98,7 +92,7 @@ def main() -> None:
     This is the main function that runs the bot
     """
     # Load the environment variables
-    #load_dotenv('.env')
+    load_dotenv('.env')
     TELEGRAMBOTTOKEN = os.getenv("TELEGRAMBOTTOKEN")
     DOWNLOAD_FOLDER = os.getenv("DOWNLOAD_FOLDER")
     updater = Updater(TELEGRAMBOTTOKEN)
@@ -117,7 +111,6 @@ def main() -> None:
 
     # Run the bot until you press Ctrl-C
     updater.idle()
-
 
 if __name__ == "__main__":
     main()
